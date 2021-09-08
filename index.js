d79185c7e13dbfc15c50e5d503d2d517bcd33f74
devSharma31 <devsharma1619@gmail.com>
2021-09-08 22:13:47 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// Import stylesheets
+import './style.css';
+// Firebase App (the core Firebase SDK) is always required
+import { initializeApp } from 'firebase/app';
+
+// Add the Firebase products and methods that you want to use
+import {getAuth, EmailAuthProvider} from 'firebase/auth';
+import {} from 'firebase/firestore';
+
+import * as firebaseui from 'firebaseui';
+
+// Document elements
+const startRsvpButton = document.getElementById('startRsvp');
+const guestbookContainer = document.getElementById('guestbook-container');
+
+const form = document.getElementById('leave-message');
+const input = document.getElementById('message');
+const guestbook = document.getElementById('guestbook');
+const numberAttending = document.getElementById('number-attending');
+const rsvpYes = document.getElementById('rsvp-yes');
+const rsvpNo = document.getElementById('rsvp-no');
+
+let rsvpListener = null;
+let guestbookListener = null;
+
+let db, auth;
+
+async function main() {
+  // Add Firebase project configuration object here
+  const firebaseConfig = {
+    apiKey: 'AIzaSyBhj0JRUBzIS_NQ_2NNLS-6BBBUzg0b2TA',
+    authDomain: 'fir-web-codelab-bb157.firebaseapp.com',
+    projectId: 'fir-web-codelab-bb157',
+    storageBucket: 'fir-web-codelab-bb157.appspot.com',
+    messagingSenderId: '530353771635',
+    appId: '1:530353771635:web:9b7fbcc773342bc55b4606'
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  auth = getAuth();
+
+  // FirebaseUI config
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // Handle sign-in.
+        // Return false to avoid redirect.
+        return false;
+      }
+    }
+  };
+
+  const ui = new firebaseui.auth.AuthUI(auth);
+}
+main();
